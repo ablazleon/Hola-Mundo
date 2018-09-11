fd231f93ce29e01808162011465298c14b50109f
Adrian <adrian@Adrians-iMac.local>
2018-09-11 13:38:39 +0200

    1

---
+++ Hola Mundo/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Hola Mundo
+//
+//  Created by Adrian on 09/09/2018.
+//  Copyright © 2018 Adrián Blázquez León. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var msgLabel: UILabel!
+    @IBOutlet weak var MKMapView: MTKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var UIBUtton: UIButton!
+    
+    @IBAction func updateHola(_ sender: UIButton) {
+        msgLabel.text = "Hola"
+    }
+    
+    
+    @IBAction func updateMundo(_ sender: UIButton) {
+        msgLabel.text = "Mundo"
+        
+        let center = CCLocationCoordinate2D(latitude: 40.452445, longitude: -3.726162)
+        let span = MKCoordinateSPan(latitudeDelta: 0.004, longitudeDelta: 0.004)
+        let reg = MKCoordinateRegion(center: center, span: span)
+        
+        mapView.setregion(reg, animated: true)
+    }
+    
+    @IBAction func updateAlpha(_ sender: UISlider) {
+        msgLabel.alpha = CGFloat(sender.value)
+        
+    }
+}
+
